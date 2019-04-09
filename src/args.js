429577d2402ebfe42de9f208249ffd161c0d0eb8
mouse_484 <escmouse484@gmail.com>
2019-04-09 02:31:53 +0000

    FORMAT,Emotion

---
+++ src/args.js
@@ -0,0 +1,28 @@
+module.exports = {
+    SPEAKER: {
+        SHOW: "show",
+        HARUKA: "haruka",
+        HIKARI: "hikari",
+        TAKERU: "takeru",
+        SANTA: "santa",
+        BEAR: "bear",
+    },
+    FORMAT: {
+        WAV: "wav",
+        OGG: "ogg",
+        MP3: "mp3",
+    },
+    EMOTION: {
+        NONE: void 0,
+        HAPPINESS: "happiness",
+        ANGER: "anger",
+        SADNESS: "sadness",
+    },
+    EMOTION_LEVEL: {
+        NONE: void 0,
+        EXTREME: "4",
+        SUPER: "3",
+        HIGH: "2",
+        NORMAL: "1",
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,39 +1,47 @@
-const SPEAKER = {
-    SHOW: "show",
-    HARUKA: "haruka",
-    HIKARI: "hikari",
-    TAKERU: "takeru",
-    SANTA: "santa",
-    BEAR: "bear",
-};
+const args = require("./args");
 
 class VoiceText {
     constructor(api_key) {
         this.api_key = api_key;
 
-        this.SPEAKER = SPEAKER;
-
         this._speaker = this.SPEAKER.SHOW;
+        this._format = this.FORMAT.WAV;
+        this._emotion = void 0;
+
         //this._pitch = 100;
         //this._speed = 100;
         //this._volume = 100;
-        //this._emotion = void 0;
+
         //this._emotion_level = void 0;
-        //this._format = this.FORMAT.WAV;
+
     }
 
     speaker(speaker) {
-        if (speaker.match(this.SPEAKER)) {
+        if (speaker.match(args.SPEAKER)) {
             this._speaker = speaker;
             return this;
         } else {
-            throw new TypeError("SPEAKER required.");
+            throw new Error("SPEAKER required.");
         }
     }
-
-
-
-
+    format(format) {
+        if (format.match(args.FORMAT)) {
+            this._format = format;
+            return this;
+        } else {
+            throw new Error("FORMAT required.");
+        }
+    }
+    emotion(emotion) {
+        if (this._speaker === args.SPEAKER.SHOW)
+            throw new Error("Emotional parameters are not available in the show.")
+        if (emotion.match(args.EMOTION)) {
+            this._format = format;
+            return this;
+        } else {
+            throw new Error("EMOTION required.");
+        }
+    }
 }
 
 module.exports = VoiceText;
