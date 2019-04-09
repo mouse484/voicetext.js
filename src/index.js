6236b610bfe558970095dae95031ff3ec45654d8
mouse_484 <escmouse484@gmail.com>
2019-04-09 02:34:40 +0000

    Emotion_LEVEl

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ class VoiceText {
         this._speaker = this.SPEAKER.SHOW;
         this._format = this.FORMAT.WAV;
         this._emotion = void 0;
+        this._emotion_level = void 0;
 
         //this._pitch = 100;
         //this._speed = 100;
         //this._volume = 100;
-
-        //this._emotion_level = void 0;
-
     }
 
     speaker(speaker) {
@@ -36,10 +34,20 @@ class VoiceText {
         if (this._speaker === args.SPEAKER.SHOW)
             throw new Error("Emotional parameters are not available in the show.")
         if (emotion.match(args.EMOTION)) {
-            this._format = format;
+            this._emotion = emotion;
             return this;
         } else {
             throw new Error("EMOTION required.");
+        }
+    }
+    emotion_level(emotion_level) {
+        if (this._speaker === args.SPEAKER.SHOW)
+            throw new Error("Emotional parameters are not available in the show.")
+        if (emotion_level.match(args.EMOTION_LEVEL)) {
+            this._emotion_level = emotion_level;
+            return this;
+        } else {
+            throw new Error("EMOTION_LEBEL required.");
         }
     }
 }
