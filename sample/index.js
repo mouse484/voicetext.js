09bca8d19dd6036e98ae4e1bebf8a11bfc006c40
mouse_484 <escmouse484@gmail.com>
2019-04-09 02:45:09 +0000

    疲れた

--- sample/index.js
+++ sample/index.js
@@ -2,4 +2,8 @@ const VoiceText = require("../src");
 
 const vt = new VoiceText(process.env.TOKEN);
 
-vt.speaker("aaa");
+vt
+    .speaker(vt.SPEAKER.HARUKA)
+    .format(vt.FORMAT.MP3)
+    .emotion(vt.EMOTION.HAPPINESS)
+    .emotion_level(vt.EMOTION_LEVEL.EXTREME);
--- src/index.js
+++ src/index.js
@@ -4,18 +4,23 @@ class VoiceText {
     constructor(api_key) {
         this.api_key = api_key;
 
+        this.SPEAKER = args.SPEAKER;
+        this.FORMAT = args.FORMAT;
+        this.EMOTION = args.EMOTION;
+        this.EMOTION_LEVEL = args.EMOTION_LEVEL;
+
         this._speaker = this.SPEAKER.SHOW;
         this._format = this.FORMAT.WAV;
         this._emotion = void 0;
         this._emotion_level = void 0;
 
-        //this._pitch = 100;
-        //this._speed = 100;
-        //this._volume = 100;
+        this._pitch = 100;
+        this._speed = 100;
+        this._volume = 100;
     }
 
     speaker(speaker) {
-        if (speaker.match(args.SPEAKER)) {
+        if (speaker.match(this.SPEAKER)) {
             this._speaker = speaker;
             return this;
         } else {
@@ -23,7 +28,7 @@ class VoiceText {
         }
     }
     format(format) {
-        if (format.match(args.FORMAT)) {
+        if (format.match(this.FORMAT)) {
             this._format = format;
             return this;
         } else {
@@ -31,9 +36,9 @@ class VoiceText {
         }
     }
     emotion(emotion) {
-        if (this._speaker === args.SPEAKER.SHOW)
+        if (this._speaker === this.SPEAKER.SHOW)
             throw new Error("Emotional parameters are not available in the show.")
-        if (emotion.match(args.EMOTION)) {
+        if (emotion.match(this.EMOTION)) {
             this._emotion = emotion;
             return this;
         } else {
@@ -41,13 +46,19 @@ class VoiceText {
         }
     }
     emotion_level(emotion_level) {
-        if (this._speaker === args.SPEAKER.SHOW)
+        if (this._speaker === this.SPEAKER.SHOW)
             throw new Error("Emotional parameters are not available in the show.")
-        if (emotion_level.match(args.EMOTION_LEVEL)) {
+        if (emotion_level.match(this.EMOTION_LEVEL)) {
             this._emotion_level = emotion_level;
             return this;
         } else {
             throw new Error("EMOTION_LEBEL required.");
+        }
+    }
+
+    pitch(pitch) {
+        if (50 <= pitch <= 200) {
+            console.log("okk");
         }
     }
 }
